2153c3899202a16ef4451345394278c51b72e36c
Ben <81422@roc-teraa.nl>
2020-06-05 19:30:46 +0200

    :construction: Timer done I think, team needs to review then finished

--- Assets/Scripts/TimerOBM.cs
+++ Assets/Scripts/TimerOBM.cs
@@ -23,7 +23,7 @@ public class TimerOBM : MonoBehaviour
         float actualTimeTakenObm = startTimeObm + Time.time;
 
         string minutes = ((int)actualTimeTakenObm / 60).ToString("00");
-        string seconds = (actualTimeTakenObm % 60).ToString(" f2");
+        string seconds = (actualTimeTakenObm % 60).ToString("f2");
         //string s = seconds.Substring(seconds.Length + 2);
         //string ms = seconds.Substring(seconds.Length - 2);
 
@@ -32,7 +32,7 @@ public class TimerOBM : MonoBehaviour
 
         if (seconds.Length != 5)
         {
-            timerTextObm.text = minutes + " :0 " + seconds;
+            timerTextObm.text = minutes + ":0" + seconds;
         }
         else
         {
