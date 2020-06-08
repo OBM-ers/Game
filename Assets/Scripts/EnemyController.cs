24ee45762e4f493787bae62cdc05624d25eaae2a
Vasco Hooiveld <vascod.hooiveld@gmail.com>
2020-06-08 12:03:46 +0200

    🐛 Fixed enemy getting stuck on wall

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,21 +6,35 @@ public class EnemyController : MonoBehaviour
 {
     public float enemySpeedObm;
     public float distanceObm;
+    public float checkRadiusObm;
 
     private bool movingRightObm = true;
 
     public Transform groundCheckObm;
+    public LayerMask wallCheckObm;
 
     void Update()
+    {
+        moveEnemyObm();
+    }
+
+    private void moveEnemyObm()
     {
         transform.Translate(Vector2.right * enemySpeedObm * Time.deltaTime);
 
         RaycastHit2D groundInfoObm = Physics2D.Raycast(groundCheckObm.position, Vector2.down, distanceObm);
-        if (groundInfoObm.collider == false) {
-            if (movingRightObm == true) {
+
+        bool m_isWallObm = Physics2D.OverlapCircle(groundCheckObm.position, checkRadiusObm, wallCheckObm);
+
+        if (groundInfoObm.collider == false || m_isWallObm == true)
+        {
+            if (movingRightObm == true)
+            {
                 transform.eulerAngles = new Vector3(0, -180, 0);
                 movingRightObm = false;
-            } else {
+            }
+            else
+            {
                 transform.eulerAngles = new Vector3(0, 0, 0);
                 movingRightObm = true;
             }
