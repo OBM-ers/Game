89ef1b4a38789712632c5fc3e5a04ee4e266543c
Vasco Hooiveld <vascod.hooiveld@gmail.com>
2020-06-08 10:47:10 +0200

    ✨ Enemy patrol movement

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float enemySpeedObm;
+    public float distanceObm;
+
+    private bool movingRightObm = true;
+
+    public Transform groundCheckObm;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * enemySpeedObm * Time.deltaTime);
+
+        RaycastHit2D groundInfoObm = Physics2D.Raycast(groundCheckObm.position, Vector2.down, distanceObm);
+        if (groundInfoObm.collider == false) {
+            if (movingRightObm == true) {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRightObm = false;
+            } else {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRightObm = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MeleeCombat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeCombat : MonoBehaviour
+{
+    public Animator animatorOBM;
+    private float timerOBM;
+    public float timeBetweenAttackOBM;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerOBM -= Time.deltaTime;
+
+        if(timerOBM <= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                AttackOBM();
+                timerOBM = timeBetweenAttackOBM;
+            }
+            
+        }
+
+        
+    }
+
+    private void AttackOBM()
+    {
+        animatorOBM.SetTrigger("attack");
+    }
+}
