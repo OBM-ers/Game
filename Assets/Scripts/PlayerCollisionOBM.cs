34e01d7d2872bcb8c6768252a445e7fa42ca9df6
Ben <81422@roc-teraa.nl>
2020-06-09 12:01:48 +0200

    :sparkles: collectibles on first map

--- Assets/Scripts/PlayerCollisionOBM.cs
+++ Assets/Scripts/PlayerCollisionOBM.cs
@@ -5,8 +5,12 @@ using UnityEngine.UI;
 
 public class PlayerCollisionOBM : MonoBehaviour
 {
+    public Text soulAmountObm;
+
     public Rigidbody2D playerObm;
     public Transform spawnObm;
+    private bool soulCollectedObm = false;
+    private int soulAmountIntegerObm = 0;
 
     void FixedUpdate()
     {
@@ -14,6 +18,24 @@ public class PlayerCollisionOBM : MonoBehaviour
         if (playerObm.position.y < -25f)
         {
             playerObm.position = spawnObm.position;
+        }
+
+        if (soulCollectedObm == true)
+        {
+            soulAmountIntegerObm++;
+            soulAmountObm.text = soulAmountIntegerObm.ToString("00");
+            soulCollectedObm = false;
+        }
+    }
+
+    //OnTriggerEnter is a static function made by unity so no Creator Identifier possible
+    public void OnTriggerEnter2D(Collider2D collectibleObm)
+    {
+        //collectibles
+        if (collectibleObm.gameObject.CompareTag("Collectible"))
+        {
+            Destroy(collectibleObm.gameObject);
+            soulCollectedObm = true;
         }
     }
 }
