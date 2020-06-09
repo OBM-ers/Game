cbc3cc771a43acf5958b8b44fdea1f5124509515
Ben <81422@roc-teraa.nl>
2020-06-09 10:27:34 +0200

    :sparkles: player can fall off map and respawn

---
+++ Assets/Scripts/PlayerCollisionOBM.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCollisionOBM : MonoBehaviour
+{
+    public Rigidbody2D playerObm;
+    public Transform spawnObm;
+
+    void FixedUpdate()
+    {
+        //Death
+        if (playerObm.position.y < -25f)
+        {
+            playerObm.position = spawnObm.position;
+        }
+    }
+}
