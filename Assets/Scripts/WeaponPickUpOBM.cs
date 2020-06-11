2f7236d9c457f54bf63c613c3538c1c256b1ae90
Ben <81422@roc-teraa.nl>
2020-06-11 14:35:46 +0200

    :construction: pickup weapon in progress

---
+++ Assets/Scripts/WeaponPickUpOBM.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPickUpOBM : MonoBehaviour
+{
+    //public WeaponOBM weaponObm;
+
+    public GameObject swordObm;
+    public GameObject scytheObm;
+
+    private void OnTriggerEnter2D(Collider2D playerObm)
+    {
+        if(playerObm.tag == "Player")
+        {
+            swordObm.SetActive(false);
+            scytheObm.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
