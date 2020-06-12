8282c648c3c5a60fe7b68f18d2a14002adef43b6
Ben <81422@roc-teraa.nl>
2020-06-12 14:48:17 +0200

    :sparkles: Can pickup weapons and drop them

--- Assets/Scripts/WeaponPickUpOBM.cs
+++ Assets/Scripts/WeaponPickUpOBM.cs
@@ -1,21 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class WeaponPickUpOBM : MonoBehaviour
 {
-    //public WeaponOBM weaponObm;
-
     public GameObject swordObm;
     public GameObject scytheObm;
 
+    public SpriteRenderer spriteRendererObm;
+    public Sprite swordSpriteObm;
+    public Sprite scytheSpriteObm;
+
+    private float timerObm = 2f;
+    private bool weaponSwitchedObm = false;
+    private bool cooldownWeaponObm = false;
+
+    void Update()
+    {
+        if (cooldownWeaponObm == true)
+        {
+            if(timerObm <= 0)
+            {
+                cooldownWeaponObm = false;
+            }
+            else
+            {
+                timerObm -= Time.deltaTime;
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D playerObm)
     {
-        if(playerObm.tag == "Player")
+        if (playerObm.tag == "Player")
         {
-            swordObm.SetActive(false);
-            scytheObm.SetActive(true);
-            Destroy(gameObject);
+            if (cooldownWeaponObm == false)
+            {
+                if (weaponSwitchedObm == false)
+                {
+                    swordObm.SetActive(false);
+                    scytheObm.SetActive(true);
+                    spriteRendererObm.sprite = swordSpriteObm;
+                    weaponSwitchedObm = true;
+                }
+                else
+                {
+                    swordObm.SetActive(true);
+                    scytheObm.SetActive(false);
+                    spriteRendererObm.sprite = scytheSpriteObm;
+                    weaponSwitchedObm = false;
+                }
+                cooldownWeaponObm = true;
+                timerObm = 2f;
+            }
         }
     }
 }
