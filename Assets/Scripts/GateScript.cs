7300695288ae1536922729d4e5e91b8081ec5dbc
Vasco Hooiveld <vascod.hooiveld@gmail.com>
2020-06-15 13:25:38 +0200

    :bento: Added gates

---
+++ Assets/Scripts/GateScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GateScript : MonoBehaviour
+{
+    //public Animator transitionAnimatorObm;
+    public string sceneNameObm;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("Scene");
+            SceneManager.LoadScene(sceneNameObm);
+        }
+    }
+}
