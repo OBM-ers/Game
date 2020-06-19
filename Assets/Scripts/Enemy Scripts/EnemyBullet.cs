d76db29e5d43699c51cecf6fe446d992357d39d8
Vasco Hooiveld <vascod.hooiveld@gmail.com>
2020-06-19 14:53:19 +0200

    :sparkles: shooting enemy added

---
+++ Assets/Scripts/Enemy Scripts/EnemyBullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float bulletSpeedObm;
+    public int bulletDamageObm;
+    public Rigidbody2D rigidbodyBulletObm;
+
+    void Start()
+    {
+        rigidbodyBulletObm.velocity = transform.right * bulletSpeedObm;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collisionObm)
+    {
+        if (collisionObm.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            collisionObm.gameObject.GetComponent<PlayerHudOBM>().TakeDamageOBM(bulletDamageObm);
+        }
+        else if (collisionObm.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
